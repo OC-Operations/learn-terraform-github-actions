0eaf297ad12deb5a4ea9d095ce9ba6d5a01b2fbb
Arun Kumar <arun@opencyon.com>
2021-08-13 17:51:23 +0530

    Changed page content

--- main.tf
+++ main.tf
@@ -38,7 +38,7 @@ resource "aws_instance" "web" {
 
   user_data = <<-EOF
               #!/bin/bash
-              echo "Hello, World" > index.html
+              echo "Hello, OpenCyon" > index.html
               nohup busybox httpd -f -p 8080 &
               EOF
 }
